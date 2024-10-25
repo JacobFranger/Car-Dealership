9a54e511a05d119c748849f61b21e12cb8cfee35
JacobFranger <jacobfranger@gmail.com>
2024-10-25 15:54:23 -0500

    adding discreption

--- src/main/java/com/plurasight/DealerShip.java
+++ src/main/java/com/plurasight/DealerShip.java
@@ -1,4 +1,24 @@
 package com.plurasight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class DealerShip {
+
+}
+public static  ArrayList<Cars>getShowroom(){
+    ArrayList<Cars>Showroom = new ArrayList<Cars>();
+    try{
+        FileReader carInventory = new FileReader("src/main/resources/inventory.csv");
+        BufferedReader bufferedReader = new BufferedReader(carInventory);
+        String input;
+        bufferedReader.readLine();
+
+        while ((input = bufferedReader.readLine())!=null){
+            String []description = input.split("|");
+            Cars newCar = new Cars(vehicle[0], vehicle[1], vehicle[3],vehicle)
+        }
+    }
+
 }
--- src/main/java/com/plurasight/Main.java
+++ src/main/java/com/plurasight/Main.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    private static Object Cars;
+
     public static void main(String[] args) {
 
 
