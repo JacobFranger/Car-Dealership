7ab8becd89d0e4ad01ddade31f0f452dd21f43b6
JacobFranger <jacobfranger@gmail.com>
2024-10-25 15:15:41 -0500

    adding code

---
+++ src/main/java/com/plurasight/Cars.java
@@ -0,0 +1,108 @@
+package com.plurasight;
+
+import java.lang.reflect.Array;
+
+public class Cars {
+    // attributes
+    private String make, model, color, vehicleType;
+    private int year, vin, odometer;
+    private double price;
+
+    //overloaded constructor, no parameters
+    public Car() {
+        this.make = "";
+        this.model = "";
+        this.color = "";
+        this.odometer = odometer;
+        this.year = year;
+        this.price = price;
+        this.vehicleType = "";
+        this.vin = vin;
+    }
+
+
+
+    //overloaded constructor, 1 parameter
+    public Car(String make) {
+        this.make = make;
+        this.model = "";
+        this.color = "";
+        this.vin = vin;
+        this.year = year;
+        this.price = price;
+        this.vehicleType = "";
+        this.odometer = odometer;
+    }
+
+
+
+    // constructor
+    public vehicle(String make, String model, String color, int year, int milage, double price) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.vin = vin;
+        this.price = price;
+    }
+
+
+
+
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+}
---
+++ src/main/java/com/plurasight/Main.java
@@ -0,0 +1,94 @@
+package com.plurasight;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        ArrayList<Cars> inventory = getCars();
+        ArrayList<Cars> cart = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        int selection = 0;
+
+
+
+         while(true) {
+            try {
+                System.out.println("Select an option by typing the number:\n" +
+                        "1.Display Products\n" +
+                        "2.Display Cart\n" +
+                        "3.Exit");
+                selection = input.nextInt();
+
+                switch (selection) {
+
+                    case 1:
+                        new Cars(Cars);
+                        System.out.println("Select an option by typing the number:\n" +
+                                "1. Add product\n" +
+                                "2. Search for a product by name\n" +
+                                "3. Search for products by a price range\n" +
+                                "4. Search for products by their department\n" +
+                                "5. Go back to the home page");
+                        int selection1 = input.nextInt();
+                        switch (selection1) {
+                            case 1:
+                                addToCart(inventory, cart);
+                                break;
+                            case 2:
+                                searchByName(inventory);
+                                break;
+                            case 3:
+                                searchByPriceRange(inventory);
+                                break;
+                            case 4:
+                                searchByDepartment(inventory);
+                                break;
+                            case 5:
+                                continue;
+                            default:
+                                System.out.println("Error. Invalid input.");
+                                break;
+                        }
+                        break;
+
+                    case 2:
+                        System.out.println(displayCart(cart));
+                        System.out.println("Select an option by typing the number:\n" +
+                                "1.Checkout\n" +
+                                "2.Remove from cart\n" +
+                                "3.Go back to home screen");
+                        int selection2 = input.nextInt();
+                        switch (selection2) {
+                            case 1:
+                                checkOut(cart);
+                                break;
+                            case 2:
+                                removeFromCart(cart);
+                                break;
+                            case 3:
+                                continue;
+                            default:
+                                System.out.println("Error. Invalid input.");
+                                break;
+                        }
+
+                        break;
+
+                    case 3:
+                        System.exit(0);
+                    default:
+                        System.out.println("Error. Invalid input.");
+                        break;
+
+                }
+
+            } catch (Exception e){
+                System.out.println("Invalid input");
+                input.nextLine();
+                e.printStackTrace();
+            }
+    }
+}
